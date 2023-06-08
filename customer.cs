6995f412e158b6fa4674ccd46444e4823cd47c78
2joaomiguel6 <102696102+2joaomiguel6@users.noreply.github.com>
2023-06-08 12:34:37 -0300

    Add files via upload

---
+++ customer.cs
@@ -0,0 +1,13 @@
+namespace ams_heranca_repository{
+class Customer
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public Customer(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+    }
+}
